733c5ab0765b9103febe235669be7b6de1d97870
Eric Ngako <ericnk1@live.com>
2020-12-16 19:50:15 +0200

    Components created

---
+++ src/app/pet-type/pet-type.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pet-type',
+  templateUrl: './pet-type.component.html',
+  styleUrls: ['./pet-type.component.css']
+})
+export class PetTypeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
