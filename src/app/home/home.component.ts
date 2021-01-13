4a3bcc8f66f71a1e6e4a44513b2f686c89641b7f
Eric Ngako <ericnk1@live.com>
2021-01-13 19:04:07 +0200

    Updated

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  google: any;
+  lat = 59.436962;
+  lng = 24.753574;
   constructor() { }
 
   ngOnInit(): void {
